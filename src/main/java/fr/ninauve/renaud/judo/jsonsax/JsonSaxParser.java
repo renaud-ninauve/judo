d3b674e4fb3ba31bed32ceeb1ab0629c32f0d6fd
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-04-05 15:13:00 +0200

    [green] parse object with strings

--- src/main/java/fr/ninauve/renaud/judo/jsonsax/JsonSaxParser.java
+++ src/main/java/fr/ninauve/renaud/judo/jsonsax/JsonSaxParser.java
@@ -11,20 +11,46 @@ public class JsonSaxParser {
   private static final char END_ARRAY = ']';
   private static final char VALUES_DELIMITER = ',';
   private static final char DOUBLE_QUOTE = '\"';
+  private static final char FIELD_NAME_VALUE_DELIMITER = ':';
+
+  private boolean expectingFieldName;
+  private boolean expectingFieldValue;
+  private String currentField;
 
   public void parse(String json, JsonSaxListener listener) {
     final StreamTokenizer tokenizer = createTokenizer(json);
     int tokenType;
     while ((tokenType = nextToken(tokenizer)) != StreamTokenizer.TT_EOF) {
-      if (tokenType == VALUES_DELIMITER || tokenType == DOUBLE_QUOTE) {
+      if (tokenType == VALUES_DELIMITER
+          || tokenType == DOUBLE_QUOTE
+          || tokenType == FIELD_NAME_VALUE_DELIMITER) {
         continue;
       }
       switch (tokenType) {
-        case START_OBJECT -> listener.startObject();
-        case END_OBJECT -> listener.endObject();
+        case START_OBJECT -> {
+          listener.startObject();
+          expectingFieldName = true;
+        }
+        case END_OBJECT -> {
+          listener.endObject();
+          expectingFieldName = false;
+        }
         case START_ARRAY -> listener.startArray();
         case END_ARRAY -> listener.endArray();
-        case StreamTokenizer.TT_WORD -> listener.stringValue(tokenizer.sval);
+        case StreamTokenizer.TT_WORD -> {
+          if (expectingFieldName) {
+            currentField = tokenizer.sval;
+            expectingFieldName = false;
+            expectingFieldValue = true;
+          } else if (expectingFieldValue) {
+            listener.stringField(currentField, tokenizer.sval);
+            expectingFieldName = true;
+            expectingFieldValue = false;
+            currentField = null;
+          } else {
+            listener.stringValue(tokenizer.sval);
+          }
+        }
         case StreamTokenizer.TT_NUMBER -> listener.numberValue(tokenizer.nval);
         default -> throw new AssertionError("tokenType " + tokenType + " is not currently handled");
       }
