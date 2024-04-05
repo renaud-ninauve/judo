126a1c33aad5b63bc0bf2efac75a97d167645e9f
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-04-05 14:28:37 +0200

    [green] parse empty object

--- src/main/java/fr/ninauve/renaud/judo/jsonsax/JsonSaxParser.java
+++ src/main/java/fr/ninauve/renaud/judo/jsonsax/JsonSaxParser.java
@@ -1,6 +1,41 @@
 package fr.ninauve.renaud.judo.jsonsax;
 
-public class JsonSaxParser {
+import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
 
-  public void parse(String json, JsonSaxListener listener) {}
+public class JsonSaxParser {
+  private static final char START_OBJECT = '{';
+  private static final char END_OBJECT = '}';
+
+  public void parse(String json, JsonSaxListener listener) {
+    final StreamTokenizer tokenizer = createTokenizer(json);
+    int tokenType;
+    while ((tokenType = nextToken(tokenizer)) != StreamTokenizer.TT_EOF) {
+      if (tokenType == START_OBJECT) {
+        listener.startObject();
+      }
+      if (tokenType == END_OBJECT) {
+        listener.endObject();
+      }
+    }
+  }
+
+  private StreamTokenizer createTokenizer(String json) {
+    StreamTokenizer tokenizer = new StreamTokenizer(new StringReader(json));
+    tokenizer.eolIsSignificant(false);
+    tokenizer.ordinaryChar('{');
+    tokenizer.ordinaryChar('}');
+    tokenizer.ordinaryChar('[');
+    tokenizer.ordinaryChar(']');
+    return tokenizer;
+  }
+
+  private int nextToken(StreamTokenizer streamTokenizer) {
+    try {
+      return streamTokenizer.nextToken();
+    } catch (IOException e) {
+      throw new IllegalStateException("failed to read next token", e);
+    }
+  }
 }
